47c7aafd554e061d8967e3ddce7b335f4f13faa4
Szalkowski <mac.szalkowski@gmail.com>
2016-04-10 16:29:38 +0200

    fixed icon in services and add blur to firefox

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 $(function(){
     $(window).scroll(function(){
         if ($(this).scrollTop() > 50){
-            $('.mast-header').css({'-webkit-filter': 'blur(7px'});
+            $('.mast-header').css({'-webkit-filter': 'blur(7px)', '-moz-filter': 'blur(7px)', 'filter': 'blur(7px)'});
         } else {
-            $('.mast-header').css({'-webkit-filter': 'blur(0px'});
+            $('.mast-header').css({'-webkit-filter': 'blur(0px)', '-moz-filter': 'blur(0px)', 'filter': 'blur(0px)' });
         }
-   lastScrollTop = st;
 });
 });
