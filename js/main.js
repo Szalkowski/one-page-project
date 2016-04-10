6a4621f00488d58ea6b5ad953f2e875bacc048a4
Szalkowski <mac.szalkowski@gmail.com>
2016-04-10 15:46:42 +0200

    shift class and add blur filter
    shift class from header to aanother container and add blur filter by
    jquery to header background

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 50){
+            $('.mast-header').css({'-webkit-filter': 'blur(7px'});
+        } else {
+            $('.mast-header').css({'-webkit-filter': 'blur(0px'});
+        }
+   lastScrollTop = st;
+});
+});
