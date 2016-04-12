c7608f467eb89f206b9ee972f4ed85e1fbe23901
Szalkowski <mac.szalkowski@gmail.com>
2016-04-12 22:22:34 +0200

    fixed all issues

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
+    new WOW().init();
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 75) {
             $('.header-img').attr("filter", "url(#blur)");
         }
         else {
