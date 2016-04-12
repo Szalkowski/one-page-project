d37f0342c320718a3ad7acf06f9dcc7b408a99e4
Szalkowski <mac.szalkowski@gmail.com>
2016-04-12 08:22:21 +0200

    swap jquery css to addClass & removeClass()

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-$(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 50){
-            $('.mast-header').css({'-webkit-filter': 'blur(7px)', '-moz-filter': 'blur(7px)', 'filter': 'blur(7px)'});
-        } else {
-            $('.mast-header').css({'-webkit-filter': 'blur(0px)', '-moz-filter': 'blur(0px)', 'filter': 'blur(0px)' });
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.mast-header').addClass('header-bg-blur');
         }
-});
+        else {
+            $('.mast-header').removeClass('header-bg-blur');
+        }
+    });
 });
